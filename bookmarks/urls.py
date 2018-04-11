dd5abf57d908c08d002de0bebf1efbc9a3d1d100
Jesse Hood <jessenoob@gmail.com>
2018-04-11 18:28:42 -0400

    added basic view of a bookmark and a delete button

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create', views.create, name='create')
+    path('create', views.create, name='create'),
+    path('edit/<str:pk>', views.edit, name='edit'),
+    path('read/<str:pk>', views.read, name='read'),
+    path('delete/<str:pk>', views.delete, name='delete')
 ]
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.decorators.http import require_http_methods, require_POST
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_http_methods, require_POST, require_GET
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from .models import Bookmark
@@ -11,8 +11,25 @@ def index(request):
 
 @require_POST
 def create(request):
-    """Takes the bookform form data and saves it to the database."""
+    """Takes the bookmark form data and saves it to the database."""
     form = BookmarkForm(request.POST)
     if form.is_valid():
         form.save()
         return HttpResponseRedirect(reverse('index'))
+
+@require_http_methods(['GET', 'POST'])
+def edit(request):
+    raise NotImplementedError()
+
+@require_GET
+def read(request, pk):
+    """Display the details of a single bookmark."""
+    bookmark = get_object_or_404(Bookmark, pk=pk)
+    context = {'bookmark': bookmark}
+    return render(request, 'bookmarks/read.html', context)
+
+@require_POST
+def delete(request, pk):
+    bookmark = get_object_or_404(Bookmark, pk=pk)
+    bookmark.delete()
+    return HttpResponseRedirect(reverse('index'))
