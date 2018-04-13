594427f99a93592a080040e10c8576b433c21f81
Jesse Hood <jessenoob@gmail.com>
2018-04-13 13:49:34 -0400

    remove debugging statement

--- djorg/settings.py
+++ djorg/settings.py
@@ -85,13 +85,7 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {}
-if True:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-else:
-    DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
