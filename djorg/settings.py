253551086fff1cfe73ddcf26aa180b31a57d0fe7
Jesse Hood <jessenoob@gmail.com>
2018-04-18 13:14:18 -0400

    modify cors policy

--- djorg/settings.py
+++ djorg/settings.py
@@ -93,30 +93,8 @@ REST_FRAMEWORK = {
 }
 
 # Cors headers
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-
-CORS_ALLOW_METHODS = (
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-)
-
-CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-)
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+CORS_ORIGIN_WHITELIST = config('ALLOWED_CLIENTS').split(',')
 
 GRAPHENE = {
     'SCHEMA': 'notes.schema.schema'
